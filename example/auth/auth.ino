75b0964e52263ec17d94ed9413a4027caace4e3e
alex-pf <prizrakfantezi@gmail.com>
2016-02-23 20:14:34 +0300

    Добавил скеч авторизация с токеном - auth

--- example/auth/auth.ino
+++ example/auth/auth.ino
@@ -1,5 +1,8 @@
 String inputString = "";         // a string to hold incoming data
 boolean stringComplete = false;  // whether the string is complete
+boolean token = false;
+unsigned long tokTime=0;
+unsigned long tokLive=10;
 
 void setup() {
   // initialize serial:
@@ -11,12 +14,19 @@ void setup() {
 void loop() {
   // print the string when a newline arrives:
   if (stringComplete) {
-    Serial.println(inputString);
-    // clear the string:
+    Serial.print(inputString);
     inputString = "";
     stringComplete = false;
   }
-  delay (3000);
+  // print Ping and token tomer if user authorized
+  if(tokTime>0 and token){
+    delay (3000);
+    Serial.print("Ping ");
+    Serial.println(tokTime);
+    tokTime--;
+  }
+  // reset token if session ended
+  if (tokTime == 0) token=false;
 }
 
 void serialEvent() {
@@ -27,8 +37,10 @@ void serialEvent() {
     inputString += inChar;
     // if the incoming character is a newline, set a flag
     // so the main loop can do something about it:
-    if (inChar == '\n') {
-      stringComplete = true;
-    }
+    if (inChar == '\n') stringComplete = true;
   }
+  // set token if user eters the password
+  if(inputString =="-p mypass\n") token = true;
+  // reset session time if user working
+  tokTime=tokLive;
 }
