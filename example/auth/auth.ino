a891bfbf3555aeb3fd4d2e7ea6b3cb910aebb13d
alex-pf <prizrakfantezi@gmail.com>
2016-03-15 19:53:02 +0300

    Добавил включение диода, во время сессии. Авторизовался - диод горит. Сессия протухла - диод погас.

--- example/auth/auth.ino
+++ example/auth/auth.ino
@@ -1,8 +1,9 @@
 #include <Regexp.h>
 
+int led = 13;
 String inputString = "";         // a string to hold incoming data
 String uPass="";
-String Password="mypass";
+String Password="1";
 boolean stringComplete = false;  // whether the string is complete
 boolean token = false;
 unsigned long tokTime=0;
@@ -14,17 +15,20 @@ boolean itsKey(char k, String inputString) {
   MatchState ms (buf);
   char kBuf[9] = "^\-?[\s]*";
   kBuf[2]=k;
-  Serial.println(kBuf);
   if (ms.MatchCount(kBuf)>0) return true;
   else return false;
 }
 
 
 void setup() {
+  // initialize the digital pin as an output.
+  pinMode(led, OUTPUT);
   // initialize serial:
   Serial.begin(9600);
   // reserve 200 bytes for the inputString:
   inputString.reserve(200);
+  Serial.println("Parametrs:");
+  Serial.println("-p <password>");
 }
 
 void loop() {
@@ -43,7 +47,10 @@ void loop() {
     tokTime--;
   }
   // reset token if session ended
-  if (tokTime == 0) token=false;
+  if (tokTime == 0) {
+    token=false;
+    digitalWrite(led, LOW);    // turn the LED off by making the voltage LOW
+  }
 }
 
 void serialEvent() {
@@ -61,7 +68,10 @@ void serialEvent() {
     uPass = inputString;
     uPass.replace("-p ","");
     uPass.trim();
-    if(uPass==Password) token = true;
+    if(uPass==Password) {
+      token = true;
+      digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
+    }
   }
   // reset session time if user working
   tokTime=tokLive;
